ac14581a4f5c9b7c449feb8779a4c2cd5091aefe
csvw <shaevw@gmail.com>
2019-03-09 13:35:21 -0800

    Removed redundant files.

--- scale.js
+++
@@ -1,9 +0,0 @@
-class Scale {
-	constructor(layout) {
-		this.layout = layout;
-		this.graphX = d3.scaleLinear().domain([0, 10]).range([0, layout.graph_width]);
-		this.graphY = d3.scaleLinear().domain([0, 10]).range([layout.height, 0]);
-	}
-}
-
-export { Scale };
